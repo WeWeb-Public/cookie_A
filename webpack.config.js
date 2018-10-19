9d9efcda14a1093b25a188205287f18d26bd43f2
Kramerbaf <nds.kram@gmail.com>
2018-10-19 16:00:58 +0200

    updated config

--- webpack.config.js
+++ webpack.config.js
@@ -2,97 +2,208 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports = [{
-    name: 'front',
-    entry: './src/index.js',
-    mode: 'production',
-    externals: {
-        'vue': 'Vue'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'webpack-strip-block',
-                options: {
-                    start: 'wwManager:start',
-                    end: 'wwManager:end'
-                }
-            },
-            // this will apply to both plain `.js` files
-            // AND `<script>` blocks in `.vue` files
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            // this will apply to both plain `.css` files
-            // AND `<style>` blocks in `.vue` files
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "front.js"
-    },
-    plugins: [
-        // make sure to include the plugin for the magic
-        new VueLoaderPlugin(),
-    ]
-},
-{
-    name: 'manager',
-    entry: './src/index.js',
-    mode: 'production',
-    externals: {
-        'vue': 'Vue'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    module: {
-        rules: [
+module.exports = function (env) {
 
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
+    if (env == 'build') {
+        return [{
+            name: 'front',
+            entry: './src/index.js',
+            mode: 'production',
+            externals: {
+                'vue': 'Vue'
             },
-            // this will apply to both plain `.js` files
-            // AND `<script>` blocks in `.vue` files
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
+            devServer: {
+                contentBase: './dist'
             },
-            // this will apply to both plain `.css` files
-            // AND `<style>` blocks in `.vue` files
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
+            module: {
+                rules: [
+                    {
+                        test: /\.vue$/,
+                        loader: 'vue-loader'
+                    },
+                    {
+                        test: /\.vue$/,
+                        loader: 'weweb-strip-block',
+                        options: {
+                            blocks: [
+                                {
+                                    start: 'wwManager:start',
+                                    end: 'wwManager:end'
+                                }
+                            ]
+                        }
+                    },
+                    // this will apply to both plain `.js` files
+                    // AND `<script>` blocks in `.vue` files
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader'
+                    },
+                    // this will apply to both plain `.css` files
+                    // AND `<style>` blocks in `.vue` files
+                    {
+                        test: /\.css$/,
+                        use: [
+                            'vue-style-loader',
+                            'css-loader'
+                        ]
+                    }
                 ]
-            }
+            },
+            output: {
+                path: path.join(__dirname, "dist"),
+                filename: "front.js"
+            },
+            plugins: [
+                // make sure to include the plugin for the magic
+                new VueLoaderPlugin(),
+            ]
+        },
+        {
+            name: 'manager',
+            entry: './src/index.js',
+            mode: 'production',
+            externals: {
+                'vue': 'Vue'
+            },
+            devServer: {
+                contentBase: './dist'
+            },
+            module: {
+                rules: [
+
+                    {
+                        test: /\.vue$/,
+                        loader: 'vue-loader'
+                    },
+                    // this will apply to both plain `.js` files
+                    // AND `<script>` blocks in `.vue` files
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader'
+                    },
+                    // this will apply to both plain `.css` files
+                    // AND `<style>` blocks in `.vue` files
+                    {
+                        test: /\.css$/,
+                        use: [
+                            'vue-style-loader',
+                            'css-loader'
+                        ]
+                    }
+                ]
+            },
+            output: {
+                path: path.join(__dirname, "dist"),
+                filename: "manager.js"
+            },
+            plugins: [
+                // make sure to include the plugin for the magic
+                new VueLoaderPlugin()
+            ]
+        }
         ]
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "manager.js"
-    },
-    plugins: [
-        // make sure to include the plugin for the magic
-        new VueLoaderPlugin()
-    ]
+    }
+    else if (env == 'dev') {
+        console.log('SERVE FRONT');
+        return [{
+            name: 'front',
+            entry: './src/index.js',
+            mode: 'development',
+            externals: {
+                'vue': 'Vue'
+            },
+            devServer: {
+                contentBase: './dist'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.vue$/,
+                        loader: 'vue-loader'
+                    },
+                    {
+                        test: /\.vue$/,
+                        loader: 'weweb-strip-block',
+                        options: {
+                            blocks: [
+                                {
+                                    start: 'wwManager:start',
+                                    end: 'wwManager:end'
+                                }
+                            ]
+                        }
+                    },
+                    // this will apply to both plain `.js` files
+                    // AND `<script>` blocks in `.vue` files
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader'
+                    },
+                    // this will apply to both plain `.css` files
+                    // AND `<style>` blocks in `.vue` files
+                    {
+                        test: /\.css$/,
+                        use: [
+                            'vue-style-loader',
+                            'css-loader'
+                        ]
+                    }
+                ]
+            },
+            output: {
+                path: path.join(__dirname, "dist"),
+                filename: "front.js"
+            },
+            plugins: [
+                // make sure to include the plugin for the magic
+                new VueLoaderPlugin(),
+            ]
+        },
+        {
+            name: 'manager',
+            entry: './src/index.js',
+            mode: 'development',
+            externals: {
+                'vue': 'Vue'
+            },
+            devServer: {
+                contentBase: './dist'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.vue$/,
+                        loader: 'vue-loader'
+                    },
+                    // this will apply to both plain `.js` files
+                    // AND `<script>` blocks in `.vue` files
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader'
+                    },
+                    // this will apply to both plain `.css` files
+                    // AND `<style>` blocks in `.vue` files
+                    {
+                        test: /\.css$/,
+                        use: [
+                            'vue-style-loader',
+                            'css-loader'
+                        ]
+                    }
+                ]
+            },
+            output: {
+                path: path.join(__dirname, "dist"),
+                filename: "manager.js"
+            },
+            plugins: [
+                // make sure to include the plugin for the magic
+                new VueLoaderPlugin(),
+            ]
+        }
+        ]
+
+    }
+
 }
-]
